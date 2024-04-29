a3eb51afc421ee150398ccad3395b9094d90f58f
Eric Ung <ungericw@tutanota.com>
2024-04-28 19:21:48 -0500

    equation1

--- InferrableLanguages/InferrableLanguages/InferrableLanguages.cs
+++ InferrableLanguages/InferrableLanguages/InferrableLanguages.cs
@@ -4,72 +4,32 @@ namespace InferrableLanguages
 {
     public static class InferrableLanguages
     { 
-        private static string SubtractFromLeft(string left, string right)
+        private static string SubtractFromRight(string left, string right)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            int i = 0;
-
-            while (i < left.Length && i < right.Length)
+            if (left.Length < right.Length)
             {
-                if (left[i] != right[i])
+                if (right.Substring(right.Length - left.Length, left.Length) == left)
                 {
-                    break;
+                    return right.Substring(0, right.Length - left.Length);
                 }
-
-                i++;
             }
-
-            while (i < right.Length)
+            else if (right.Length < left.Length)
             {
-                stringBuilder.Append(right[i++]);
+                if (left.Substring(0, right.Length) == right)
+                {
+                    return left.Substring(left.Length - right.Length, right.Length);
+                }
             }
 
-            return stringBuilder.ToString();
+            return string.Empty;
         }
 
-        private static (int, string) SubtractFromRight(string left, string right)
+        private static string AddLeft(string left, string right)
         {
-            StringBuilder stringBuilder = new StringBuilder();
-            int i = left.Length - 1;
-            int j = right.Length - 1;
+            StringBuilder stringBuilder = new StringBuilder(left);
+            stringBuilder.Append(right);
 
-            if (i < j)
-            {
-                while (i >= 0 && j >= 0)
-                {
-                    if ((left[i] != right[j]))
-                    {
-                        break;
-                    }
-                    i--;
-                    j--;
-                }
-
-                while (i >= 0)
-                {
-                    stringBuilder.Append(left[i--]);
-                }
-
-                while (j >= 0)
-                {
-                    stringBuilder.Append(right[j--]);
-                }
-                
-                StringBuilder reversed = new StringBuilder();
-                String str = stringBuilder.ToString();
-
-                for (int k = str.Length - 1; k >= 0; k--)
-                {
-                    reversed.Append(str[k]);
-                }
-
-                return (1, reversed.ToString());
-            }
-            else
-            {
-                return (-1, SubtractFromLeft(left, right));
-            }
-
+            return stringBuilder.ToString();
         }
 
         // The LHS equations can be generalized to LHS = a - b + c - d + e, 
@@ -84,51 +44,14 @@ namespace InferrableLanguages
         // Stacks would require more more stacking, what of other operations besides + and -?
         // Replace is generalized
 
-        public static string LHSx1(string y1, string z2, string x2, string y2, string z1)
+        public static string Equation1(string y1, string z2, string x2, string y2, string z1)
         {
-            StringBuilder x1Sb = new StringBuilder();
+            var cur = SubtractFromRight(y2, z1);
+            var cur2 = AddLeft(x2, cur);
+            var cur3 = SubtractFromRight(z2, cur2);
+            var cur4 = AddLeft(y1, cur3);
 
-            x1Sb = new StringBuilder(SubtractFromRight(y1, z2).Item2);
-            x1Sb.Append(x2);
-            x1Sb = new StringBuilder(SubtractFromRight(x1Sb.ToString(), y2).Item2);
-            x1Sb.Append(z1);
-
-            return x1Sb.ToString();
-        }
-
-        public static string LHSy1(string z2, string x2, string y2, string z1, string x1)
-        {
-            StringBuilder y1Sb = new StringBuilder();
-
-            var sfrz2x2 = SubtractFromRight(z2, x2);
-
-            y1Sb = new StringBuilder(sfrz2x2.Item2);
-            y1Sb.Append(y2);
-            var sfry1Sbx2 = SubtractFromRight(y1Sb.ToString(), z1);
-            y1Sb = new StringBuilder(sfry1Sbx2.Item2);
-            y1Sb.Append(x1);
-            
-            return y1Sb.ToString();
-        }
-
-        public static string LHSz1(string x2, string y2, string z1, string x1, string y1)
-        {
-            StringBuilder z1Sb = new StringBuilder();
-
-            (int p, string ret) = SubtractFromRight(x2, y2);
-            z1Sb = new StringBuilder(ret);
-            if (p < 0)
-            {
-                var retv= SubtractFromRight(z1, ret);
-                z1Sb = new StringBuilder(retv.Item2);
-            }
-            z1Sb.Append(z1);
-            var sfrz1Sbx1 = SubtractFromRight(z1Sb.ToString(), x1);
-            z1Sb = new StringBuilder(sfrz1Sbx1.Item2);
-            z1Sb.Append(y1);
-            
-            return z1Sb.ToString();
-
+            return cur4;
         }
     }
 }
--- InferrableLanguages/InferrableLanguagesConsole/Program.cs
+++ InferrableLanguages/InferrableLanguagesConsole/Program.cs
@@ -5,6 +5,5 @@ string x2 = "ab";
 string y2 = "a";
 string z2 = "b";
 
-Console.WriteLine(InferrableLanguages.InferrableLanguages.LHSx1(y1, z2, x2, y2, z1));
-Console.WriteLine(InferrableLanguages.InferrableLanguages.LHSy1(z2, x2, y2, z1, x1));
-Console.WriteLine(InferrableLanguages.InferrableLanguages.LHSz1(x2, y2, z1, x1, y1));
+// Console.WriteLine(InferrableLanguages.InferrableLanguages.Equation1(y1, z2, x2, y2, z1));
+Console.WriteLine(InferrableLanguages.InferrableLanguages.Equation1(x2, y2, z1, x1, y1));
