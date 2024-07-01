c41e92298454257a4bf349fee5cc205f4577602c
ericung <ungericw@tutanota.com>
2024-07-01 08:28:06 -0500

    zeta language math.net

---
+++ ZetaLanguage/ZetaLanguage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Evaluate a special function
+        Console.WriteLine(SpecialFunctions.Erf(0.5));
+
+        // Solve a random linear equation system with 500 unknowns
+        var m = Matrix<double>.Build.Random(500, 500);
+        var v = Vector<double>.Build.Random(500);
+        var y = m.Solve(v);
+        Console.WriteLine(y);
+    }
+}
